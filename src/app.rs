b1d06ebd16240b858c3a76aca6c6119e62f0892b
shinyaigeek <PhilisPaxil@gmail.com>
2021-04-17 17:59:56 +0900

    initial commit

---
+++ src/app.rs
@@ -0,0 +1,29 @@
+use yew::prelude::*;
+use wasm_bindgen::prelude::*;
+
+pub struct App {}
+
+pub enum Msg {}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        App {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <p class="bg-red-500 text-5xl">{ "Hello tailwind css!" }</p>
+        }
+    }
+}
